9383a6b96d78320059556cf7d12a1c57f88a75c7
Miguel Angel <rmiguelangel222@gmail.com>
2025-11-13 23:20:33 +0100

    Modificacion de archivo validacion.js

--- Tarea3/validacion.js
+++ Tarea3/validacion.js
@@ -1,2 +1,18 @@
-const usuario = document.getElementById('usuario');
-const valorUsuario = usuario.value
+let usuario = document.getElementById('usuario');
+usuario.outerHTML
+
+usuario.addEventListener('change', f=>{
+    if (usuario.value.length < 3) {
+        mostrarError(usuario, 'El usuario debe contener al menos 3 letras!')
+    } else {
+        
+    }
+});
+
+function mostrarError(elemento, mensaje){
+    let elementoSpan = elemento.parentElement;
+    let errorDisplay =  elementoSpan;
+
+    elementoSpan.appendChild(errorDisplay);
+    elementoSpan.textContent = mensaje;
+};
